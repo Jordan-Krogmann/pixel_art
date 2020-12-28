eea5fd5fdf580cee83c03675dc5e418cbb0b4c32
Jordan <52896238+Jordan-Krogmann@users.noreply.github.com>
2020-12-28 18:50:37 -0500

    added script

---
+++ R/pixel_art.R
@@ -0,0 +1,8 @@
+# Installation
+# devtools::install_github("privefl/pixelart")
+
+# Run Shiny app for pixel art models
+pixelart::run_pixelart()
+
+# Run Shiny app for resizing an image
+pixelart::run_resize()
